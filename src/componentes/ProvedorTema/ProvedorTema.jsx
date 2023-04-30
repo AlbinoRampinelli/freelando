56a153c68817280d9d771bbcce5324fbae712040
Albino Rampinelli <albino@dds.com.br>
2023-04-30 11:13:24 -0300

    temificando estilos globais

--- src/componentes/ProvedorTema/ProvedorTema.jsx
+++ src/componentes/ProvedorTema/ProvedorTema.jsx
@@ -30,7 +30,8 @@ const tema = {
     espacamentos: {
        s : '16px',
        l : '32px',
-    }
+    },
+    fontFamily: "'Monserrat', sans-serif" 
 }
 
 export const ProvedorTema = ({ children }) => {
